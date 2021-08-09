d74c685ed51266e7d8851926d262d414da85583e
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2021-08-09 11:11:47 +0530

    64 ms

---
+++ solutions/15. 3Sum.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums)
+    {
+    
+     /* first time solving: 09.08.2021
+     
+      Brute force approach will be: using three loops to traverse the array 
+      to find the required the output. after finding three indices where 
+      i!= j!= k ,we need to insert theses three indices in hashmap
+      
+      in next iteration if we found new three indices ,we need to check 
+      whether atleast two indices are not seen/encountered for answer before.
+      i.e search the selected three indices in hashmap, if more than one 
+      index found in map then we can't count current three as valid indices because 
+      duplicates are not allowed
+      
+      testcase 1 : [-1,0,1,2,-1,-4]
+      output:      [[-1,-1,2],[-1,0,1]]
+      here, second triplet is [-1,0,1] indices are: 0,1,2
+      insert these indices in hashmap and run algo again
+      second triplet we found : [0,-1,1] indices are: 1,2,4
+      if we search these three indices in map we found atmost two indices 
+      so these triplets are not valid so that they are not in actual output */
+        
+     /* better approach : 01
+      additional array to copy input
+      sort that array
+      select one index i, use two pointer approach to find remaining two indices.
+      time :   O(n2)        space : o(1);
+      */
+      
+       int n = nums.size();
+        if(n<3) return {};
+        vector< vector<int> > result;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i< n-2;i++)
+        {
+            if(i == 0 ||  nums[i] != nums[i-1])
+            {
+               int j = i+1, k= n-1;
+               while(j<k)
+               {
+                   int sum = nums[j] + nums[k] + nums[i];
+                   if( sum == 0) 
+                   {
+                       result.push_back( {nums[i],nums[j],nums[k]});
+                       while(j<k && nums[j] == nums[j+1]) j++;
+                       while(j<k && nums[k] == nums[k-1]) k--;
+                       j++;
+                       k--;
+                   }
+                   else if(sum>0) k--;
+                   else j++;
+               }
+            }
+        }
+        return result;
+        
+    }
+};
