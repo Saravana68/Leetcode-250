ec7abf268f2ed5b74c6f60470af383a45c88cd64
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2021-08-10 07:06:54 +0530

    Update 977. Squares of a Sorted Array.cpp

--- solutions/977. Squares of a Sorted Array.cpp
+++ solutions/977. Squares of a Sorted Array.cpp
@@ -14,6 +14,18 @@ public:
      03.handle the case middle element if size of array is odd
      time: o(n)  space: O(1);
   */
+   
+  /* bounds_error
+     if(index = -1 ) break ; written in line : 48?
+     => dry run the testcase: [7,-3,2,3,11] 
+     => in last iteration where left and right values are : 02
+        a. condition falls in third if condition
+        b. after 4^2 = 16 inserted in index =0 , index -- occurs 
+        c. this leads to bounds_error where negative index is not acceptable
+        d. for that if (index == -1 ) that means we have to inserted all the input array and unneccesarily we're trying to insert one element again in negative index
+        e. so we break the loop
+   */
+     
         
        int n  = nums.size();
         vector<int> answer(n,0);
@@ -42,7 +54,7 @@ public:
               {
                answer[index] = pow(nums[left],2);
                index--;
-               if(index == -1) break;
+               if(index == -1) break; // handling the bounds error. 
                answer[index] = pow(nums[right],2);
                left++;
                right--;
