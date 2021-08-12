fb74ce09fa31adebf754324afe38e419b8d92ff4
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2021-08-12 17:51:05 +0530

    Update 380. Insert Delete GetRandom O(1).cpp

--- solutions/380. Insert Delete GetRandom O(1).cpp
+++ solutions/380. Insert Delete GetRandom O(1).cpp
@@ -1,34 +1,52 @@
 class RandomizedSet {
-    
-    
-    
-  /* different Data structure approaches :
-  
-                  Insertion   Deletion     getRand
-             
-  vector       :  O(1)        O(N)            O(1) 
-  set          :  O(1)        O(1)            O(N)
-Doubly llist   :  O(1)        O(N)            O(N)
-    
-    */
-    
-    /* Best approach : arrays + Map;  */
-     
-​
+    
+    
+    
+  /* different Data structure approaches :
+  
+                  Insertion   Deletion     getRand
+             
+  vector       :  O(1)        O(N)            O(1) 
+  set          :  O(1)        O(1)            O(N)
+Doubly llist   :  O(1)        O(N)            O(N)
+    
+    */
+    
+    /* Best approach : arrays + Map;  */
+     
+
 public:
-    vector<int> v;
-    unordered_map<int,int> myMap;
-    RandomizedSet() {
-        
-    }
-    
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    bool insert(int val) {
-        
-        if(myMap.find(val)!= myMap.end())
-            return false;
-        v.push_back(val);
-        myMap[val] = v.size()-1;
-        return true;
-    
-    }
+    vector<int> v;
+    unordered_map<int,int> myMap;
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        
+        if(myMap.find(val)!= myMap.end())
+            return false;
+        v.push_back(val);
+        myMap[val] = v.size()-1;
+        return true;
+    
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if(!myMap.count(val))
+            return false;
+        
+        myMap[v.back()] = myMap[val];
+        swap(v.back(),v[myMap[val]]);
+        v.pop_back();
+        myMap.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+           return v[rand() % v.size()];
+    }
+};
