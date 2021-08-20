c35a017c39abcbe15607f60183335d0c6b4183ee
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2021-08-20 08:26:33 +0530

    N/A

---
+++ solutions/55. Jump Game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+    /* bruteforce approach:
+       01.use backtracking
+       02.but it will be tle
+       03. since at time of solving this problem idk dp and memoization like concepts my only bruteforce 
+       approach is backtracking
+    */
+    
+    /* optimal approach:
+       01.if we need to reach end ,then any of element's value should reach end
+       02.each element can go to i+ nums[i] step max
+       03.at any point of iteration if i > reachable then we can't fo further this is because of zero 
+       had been encountered.
+       Time: O(N) space: O(1
+​
+    */
+        int n = nums.size();
+        int maxReachable = 0;
+        for(int i =0; i<nums.size()-1; i++)
+        {
+            if(i<maxReachable)
+                return false;
+            
+            int tempReachable = i+nums[i];
+            maxReachable = max(maxReachable,tempReachable);
+        }
+        return true;
+    }
+};
