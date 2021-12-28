b1650067f8674e9af443144af4d5bf56d4160e61
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2021-12-28 07:21:30 +0530

    4 ms

---
+++ solutions/300. Longest Increasing Subsequence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int find(vector<int>&nums,int val)
+    {
+        int l=0;
+        int r=nums.size()-1;
+        int index=0;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(nums[mid]==val)
+            {
+                return mid;
+            }
+            else if(nums[mid]<val)
+            {
+                l=mid+1;
+            }
+            else
+            {
+                index=mid;
+                r=mid-1;
+            }
+        }
+        return index;
+    }
+    int lengthOfLIS(vector<int>& nums) 
+    {
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(ans.back()<nums[i])
+            {
+                ans.push_back(nums[i]);
+            }
+            else
+            {
+                int index=find(ans,nums[i]);
+                ans[index]=nums[i];
+            }
+        }
+        return ans.size();
+    }
+};
