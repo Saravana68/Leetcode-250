27c5f7591411d1ff8f245b9c0caf34775d52f7d3
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2022-04-22 11:22:36 +0530

    28 ms

--- solutions/347. Top K Frequent Elements.cpp
+++ solutions/347. Top K Frequent Elements.cpp
@@ -1,15 +1,48 @@
 ​
 class Solution {
 public:
+    /*
+       BRUTE FORCE SOLUTION :
+       --------------------
+       01. sort the elements in ASCENDING ORDER
+       02. traverse the sorted array and do the following
+       03. push first elements in answer array and push it also in unordered_set and k--
+       04. In nxt iteration check num[i] is present in unordered_set 
+       05. if yes ignore dont push it answer array else push it and do k -- 
+       06. if K becomes zero break the loop and return answer array
+       
+       TIME COMPLEXTIY  : 0(NLOGN)
+       SPACE COMPLEXITY : O(N) 
+     
+    */
+    
+    
+    /*
+        OPTIMAL SOLUTION 
+        ----------------
+        
+        01.Refer below comments 
+        
+        TIME COMPLEXITY : 0(NLOGk)
+        SPACE COMPLIXTY : O(N)
+        
+    */
+    
     #define p pair<int,int>
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         vector<int> answer;
         unordered_map<int,int> mp;
-        for(int i = 0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
         priority_queue<p,vector<p>,greater<p>> pq;
+        
+        // storing frequency in O(N) 
+        for(int i = 0;i<nums.size();i++)
+            mp[nums[i]]++;
+        
+        // I am used unordered_map so I can't iterate over map so I used to traverse nums [] 
+        // no of push_operation in heap = no.of unique elements in given array
+        // heap size will be k
+        // NlogK wil be time complexity
         for(int i =0;i<nums.size();i++){
             if(mp.count(nums[i])){
             pq.push({mp[nums[i]],nums[i]});
@@ -18,10 +51,3 @@ public:
             if(pq.size() > k) 
                 pq.pop();
         }
-        while(pq.size()){
-            answer.push_back(pq.top().second);
-            pq.pop();
-        }
-        return answer;
-    }
-};
