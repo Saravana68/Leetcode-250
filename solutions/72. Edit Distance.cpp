14a9d63af070fa95bac18913b2e8618f1ae5b491
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2022-07-19 09:48:22 +0530

    19 ms

--- solutions/72. Edit Distance.cpp
+++ solutions/72. Edit Distance.cpp
@@ -1,33 +1,26 @@
+​
 class Solution {
+    
 public:
+    int f(int i, int j, string &s1, string &s2,vector<vector<int>> &dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(s1[i] == s2[j])
+            return f(i-1,j-1,s1,s2,dp);
+        else{
+            int deletion = f(i-1,j,s1,s2,dp);
+            int replace = f(i-1,j-1,s1,s2,dp);
+            int insert = f(i,j-1,s1,s2,dp);
+            return dp[i][j] = 1 + min({deletion,replace,insert});
+        }
+    }
     int minDistance(string word1, string word2) {
         
-        int m = word1.size();
-        int n = word2.size();
-        
-        if(m == 0)
-            return n;
-        if(n ==0)
-            return m;
-        
-        int dp[m+1][n+1];
-        
-        for(int i =0;i<=m;i++){
-            for(int j =0;j<=n;j++){
-                
-                if(i==0 )
-                    dp[i][j] = j;
-                else if(j ==0)
-                    dp[i][j] = i;
-                
-                else if(word1[i-1] == word2[j-1])
-                     dp[i][j] = dp[i-1][j-1];
-                else 
-                    dp[i][j] = 1+ min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]});
-            }
-        }
-        
-        return dp[m][n];
-        
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(n-1,m-1,word1,word2,dp);
     }
 };
