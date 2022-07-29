b5a033620085ab8fc327ae128d3dc6a1cc07db9e
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2022-07-29 10:04:05 +0530

    11 ms

--- solutions/139. Word Break.cpp
+++ solutions/139. Word Break.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    bool f(string s,int i , unordered_set<string> &dict,vector<int> &dp){
-        if(i == s.size())
-            return true;
-        if(dp[i] != -1 ) return dp[i];
-        string temp ="";
-        
-        for(int idx = i;idx<s.size();idx++){
-            temp.push_back(s[idx]);
-            //cout<<temp<<endl;
-            if(dict.count(temp) && f(s,idx+1,dict,dp))
-                return dp[i] = true;
-        }
-        return dp[i] = false;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1,false);
+        dp[s.size()] = true;
+        int n = s.size();
         unordered_set<string> dict;
-        vector<int> dp(s.size(),-1);
         for(auto str : wordDict)
             dict.insert(str);
         
-        return f(s,0,dict,dp);
+        for(int i = n-1;i>=0;i--){
+            string temp ="";
+          for(int idx = i;idx<s.size();idx++){
+            temp.push_back(s[idx]);
+            if(dict.count(temp) && dp[idx+1]){
+                 dp[i] = true;
+                break;
+            }
+        }
+        }
+        return dp[0];
     }
 };
