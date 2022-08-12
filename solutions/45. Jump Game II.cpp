b31df8054d713a8e8d3a399af833c77395bc042d
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2022-08-12 19:05:47 +0530

    191 ms

--- solutions/45. Jump Game II.cpp
+++ solutions/45. Jump Game II.cpp
@@ -1,3 +1,8 @@
+/* RECURSION + MEMOIZATION 
+   -----------------------
+   TIME COMPLEXITY : O(N) - Every subproblem solved only once because of storing answer in dp[]
+   SPACE COMPLEXITY : O(N) + O(N) using dp[] + some auxillary stack space required thats why.
+*/ 
 class Solution {
 public:
     int f(int idx , int dest, vector<int> &nums,vector<int> &dp){
@@ -18,5 +23,41 @@ public:
         int n = nums.size();
         vector<int> dp(n+1,-1);
         return f(0,n-1,nums,dp);
+    }
+};
+​
+/* TABULATION
+   ----------
+   In recursion we started from index 0 now start from index n-2
+   Index n-1 is base case because we dont want jump further from target  so dp[n-1] = 0
+   if nums[i] == 0 we can't jump further. 
+   TIME COMPLEXITY : O(N)
+   SPACE COMPLEXITY : O(N)
+*/
+class Solution {
+public:
+​
+int jump(vector<int>& nums) {
+        int n = nums.size();        
+    
+        vector<int> dp(n+1,0);
+        dp[n-1] = 0;  // base case
+        
+        for(int i = n-2;i>=0;i--){
+            
+            if(nums[i] == 0 ) { // can't reach target from this cell, so discard this path
+                dp[i] = INT_MAX - 10000; // 1 + dp[i+k] if dp[i+k] is 1e9 addition not possible  thats why instead of INT_MAX some meaningful value stored
+                 continue;
+              }
+            int ans = INT_MAX;
+            int limit = nums[i];
+            for(int k = 1;k<=limit;k++){
+                if(i+k >=n) break;
+                int temp = 1+dp[i+k];
+                ans = min(ans,temp);
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
     }
 };
