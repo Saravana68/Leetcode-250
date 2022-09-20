099a33fd5022b329afb9b215197498d58e174209
SaravanaKumar <63772127+Saravana68@users.noreply.github.com>
2022-09-20 11:37:25 +0530

    32 ms

--- solutions/229. Majority Element II.cpp
+++ solutions/229. Majority Element II.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int num1  = -1,num2 = -1;
-        int cnt1 = 0, cnt2 = 0;
-        vector<int> ans;
+        vector<int> answer;
+        int num1 = -1;
+        int num2 = -1;
+        int cnt1 = 0 , cnt2 = 0;
         for(int i =0;i<nums.size();i++){
-            if(nums[i] == num1) cnt1++;
-            else if(nums[i]  == num2) cnt2++;
+            if(num1 == nums[i]) cnt1++;
+            else if(num2 == nums[i]) cnt2++;
             else if(cnt1 == 0){
-                num1 = nums[i]; cnt1 = 1;
+                num1 = nums[i];
+                cnt1 = 1;
             }
-            else if(cnt2 == 0){
-                num2 = nums[i]; cnt2 = 1;
+            else if(cnt2 ==0){
+                num2 = nums[i];
+                cnt2 = 1;
             }
             else {
                 cnt1--;
                 cnt2--;
             }
         }
+        cout<<cnt1<<cnt2<<endl;
+        cout<<num1<<" "<<num2<<endl;
         cnt1 = 0;
         cnt2 = 0;
         for(int i = 0;i<nums.size();i++){
-            if(nums[i] == num1) cnt1++;
-            else if(nums[i] == num2) cnt2++;
+            if(num1 == nums[i]) cnt1++;
+            else if(num2 == nums[i]) cnt2++;
         }
-        if(cnt1 > nums.size()/3) ans.push_back(num1);
-        if(cnt2 > nums.size()/3) ans.push_back(num2);
-        return ans;
+        cout<<cnt1<<cnt2<<endl;
+        if(cnt1 > nums.size()/3) answer.push_back(num1);
+        if(cnt2 > nums.size()/3 ) answer.push_back(num2);
+        return answer;
     }
 };
