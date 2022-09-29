84167181a149f91279ff7a177f8a0b152811d358
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2022-09-29 14:14:00 +0530

    80 ms

--- solutions/56. Merge Intervals.cpp
+++ solutions/56. Merge Intervals.cpp
@@ -1,3 +1,31 @@
+/*
+TIME : O(NlogN) SPACE :O(N)
+--------------- ----------
+vector<vector<int>> merge(vector<vector<int>>& arr) {
+        
+        vector<vector<int>> answer;
+        sort(arr.begin(),arr.end());
+        vector<int> prev = {arr[0][0],arr[0][1]};
+        
+        for(int i = 1;i<arr.size();i++){
+           if(arr[i][0] <= prev[1]){
+               prev[0]= min(prev[0],arr[i][0]);
+               prev[1] = max(prev[1],arr[i][1]);
+           }
+           else{
+               answer.push_back(prev);
+               prev[0] = arr[i][0];
+               prev[1] = arr[i][1];
+           }
+        }
+        answer.push_back(prev);
+        return answer;
+    }
+*/
+​
+​
+// O(1) SPACE SOLUTION 
+​
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
