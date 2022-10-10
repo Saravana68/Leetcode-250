4190c8129c2228089b11286aaca524b2f8770bb4
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2022-10-10 22:39:52 +0530

    401 ms

--- solutions/523. Continuous Subarray Sum.cpp
+++ solutions/523. Continuous Subarray Sum.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> m={{0,-1}};
-        int sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            int rem=sum%k;
-            if(!m.count(rem)) m[rem]=i;
-            else if(i-m[rem]>1) return true;
+        unordered_map<int,int> mp;
+        int curr_sum = nums[0];
+        mp[curr_sum % k] = 0;
+        
+        for(int i = 1;i<nums.size();i++){
+            curr_sum += nums[i];
+            if(curr_sum % k == 0) return true;
+            if(mp.count(curr_sum % k)){
+                if(i - mp[curr_sum % k] > 1) return true;
+            }
+            else mp[curr_sum % k] = i;
         }
-        return false;
+    return false;
     }
+    
 };
