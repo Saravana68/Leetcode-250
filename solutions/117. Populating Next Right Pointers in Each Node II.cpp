919ac8d8a75bf8c1a8fd00dd27e6d985f1f04304
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2022-10-13 19:19:56 +0530

    42 ms

--- solutions/117. Populating Next Right Pointers in Each Node II.cpp
+++ solutions/117. Populating Next Right Pointers in Each Node II.cpp
@@ -1,27 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+​
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+​
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+​
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+​
 class Solution {
 public:
     Node* connect(Node* root) {
        Node* curr = root;
-       Node* rowHead = NULL,*rowTail = NULL;
-       while(curr){
-           vector<Node*> children = {curr->left,curr->right};
-           for(auto temp : children){
-               if(!temp)
-                   continue;
-               if(!rowHead){
-                   rowHead = rowTail = temp;
-               }
-               else{
-                   rowTail->next = temp;
-                   rowTail = rowTail->next;
-               }
-           }
-           curr = curr->next;
-           if(!curr){
-               curr = rowHead;
-               rowHead = rowTail = NULL;
-           }
-       }
+        Node* rowHead = NULL, *rowTail = NULL;
+        while(curr){
+            vector<Node*> child ={curr->left,curr->right};
+            for(auto temp : child){
+                if(!temp) continue;
+                if(!rowHead) rowHead = rowTail = temp;
+                else{
+                    rowTail->next = temp;
+                    rowTail = rowTail->next;
+                }
+            }
+            curr = curr->next;
+            if(!curr){
+                curr = rowHead;
+                rowHead = rowTail = NULL;
+            }
+        }
         return root;
     }
 };
