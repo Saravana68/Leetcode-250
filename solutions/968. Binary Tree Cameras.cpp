8d3a83dda2645e5a754f4b407a2d49e33598b9e1
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2022-10-13 18:25:56 +0530

    31 ms

---
+++ solutions/968. Binary Tree Cameras.cpp
@@ -0,0 +1,32 @@
+// TIME : O(N) // SPACE : O(N)  13.10.2022
+class Solution {
+public:
+    void solve(TreeNode*root,unordered_set<TreeNode*> &st,TreeNode* parent,int &cam){
+        if(!root) return;
+        solve(root->left,st,root,cam);
+        solve(root->right,st,root,cam);
+        
+        // if only one node present then its parent is NULL & root is not yet covered
+        bool flag1 = (parent == NULL) && (!st.count(root));
+        
+        // if any left or right node not covered then covering that via current root is must
+        bool flag2 = (!st.count(root->left) || !st.count(root->right));
+        
+        // if any two conditions true in above statements add camera.
+        if(flag1 || flag2){
+            cam++;
+            st.insert(root);
+            st.insert(root->left);
+            st.insert(root->right);
+            st.insert(parent);
+        }
+        
+    }
+    int minCameraCover(TreeNode* root) {
+        unordered_set<TreeNode*> st;
+        st.insert(NULL);
+        int cam = 0;
+        solve(root,st,NULL,cam);
+        return cam;
+    }
+};
