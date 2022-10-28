c1d48bfaee60b8e356a2b544efd7a461d079418a
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2022-10-28 17:53:59 +0530

    42 ms

--- solutions/130. Surrounded Regions.cpp
+++ solutions/130. Surrounded Regions.cpp
@@ -1,58 +1,43 @@
 class Solution {
-public: 
-    bool seen;
-    
-    void dfs(vector<vector<char>> & board,vector<vector<bool>>&vis,int i,int j,int r,int c)
-    {
-       if(i<0 || i>=r || j<0 || j>=c || board[i][j] == 'X' || vis[i][j])
-           return ;
+public:
+    void dfs(int i, int j, vector<vector<char>> &board,vector<vector<bool>> &vis,bool &flag){
+        
+        if( i < 0 || j <0 || i >= board.size() || j>=board[0].size() || board[i][j] == 'X'|| vis[i][j])
+            return ;
         
         vis[i][j] = true;
+        if(i ==0 || j ==0 || i >= board.size()-1 || j >= board[0].size()-1) 
+            flag = true;
+            
         
-        if(i<=0 || j<=0 || i>=r-1 || j>=c-1)
-            seen = true;
-        
-        dfs(board,vis,i-1,j,r,c);
-        dfs(board,vis,i+1,j,r,c);
-        dfs(board,vis,i,j-1,r,c);
-        dfs(board,vis,i,j+1,r,c);
+        dfs(i+1,j,board,vis,flag);
+        dfs(i-1,j,board,vis,flag);
+        dfs(i,j+1,board,vis,flag);
+        dfs(i,j-1,board,vis,flag);
     }
-    void mark(vector<vector<char>> & board,int i,int j,int r,int c){
-        
-        if(i<0 || j<0 || i>=r || j >=c || board[i][j] == 'X')
-             return ;
-      
+    void mark(int i,int j,vector<vector<char>> &board){
+        if(i <0 || j <0 || i>= board.size() || j>=board[0].size() || board[i][j] =='X')
+            return;
         
         board[i][j] = 'X';
         
-         
-        mark(board,i-1,j,r,c);
-        mark(board,i+1,j,r,c);
-        mark(board,i,j-1,r,c);
-        mark(board,i,j+1,r,c);
+        mark(i+1,j,board);
+        mark(i-1,j,board);
+        mark(i,j+1,board);
+        mark(i,j-1,board);
     }
     void solve(vector<vector<char>>& board) {
         
-        int r = board.size();
-        if(r<=1) return ;
-        int c = board[0].size();
-        if(c<=1) return ;
-        
-        vector<vector<bool>> vis (r,vector<bool>(c,false));
-        
-        for(int i =1;i<r-1;i++){
-            for(int j =1 ;j<c-1;j++){
-                
-                if(board[i][j] == 'O' && !vis[i][j]) {
-                    seen = false;
-                    dfs(board,vis,i,j,r,c);
-                    if(seen == false)
-                        mark(board,i,j,r,c);
-                    seen = true;
-                }
+        vector<vector<bool>> vis(board.size(),vector<bool>(board[0].size(),false));
+        for(int i = 1;i<board.size()-1;i++){
+            for(int j = 1;j<board[0].size()-1;j++){
+                 if(!vis[i][j] && board[i][j] == 'O'){
+                     bool flag = false;
+                     dfs(i,j,board,vis,flag);
+                     if(!flag)
+                         mark(i,j,board);
+                 }
             }
         }
-        
-        
     }
 };
