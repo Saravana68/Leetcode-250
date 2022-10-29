8a0565afa048fef845a505fda160715302137d13
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2022-10-29 19:48:58 +0530

    88 ms

--- solutions/207. Course Schedule.cpp
+++ solutions/207. Course Schedule.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    bool dfs(int src,vector<int> adj[],vector<int> &recSt,vector<int> &visited){
-        
-        visited[src] =1;
-        recSt[src] = 1;
-        
-        for(auto v : adj[src]){
-            if(visited[v] == false){
-                if(!dfs(v,adj,recSt,visited))
-                    return false;
+    
+    bool  dfs(int u, vector<int> &vis, vector<int> &rec, vector<int> adj[]){
+        vis[u] = 1;
+        rec[u] = 1;
+        for(int v : adj[u]){
+            if(vis[v] == false){
+                if(dfs(v,vis,rec,adj)) return true;
             }
-            else if(recSt[v])
-                return false;
-                
+            else if(rec[v] == 1) return true;
         }
-        recSt[src] = 0;
+        rec[u] = 0;
+        return false;
+    }
+    bool canFinish(int N, vector<vector<int>>& arr) {
+        vector<int> adj[N];
+        for(vector<int> it : arr){
+            adj[it[0]].push_back(it[1]);
+        }
+        for(auto it : adj){
+            for(auto x: it)
+                cout<<x<<" ";
+            cout<<endl;
+        }
+        vector<int> vis(N,0);
+        vector<int> rec(N,0);
+        for(int i =0;i<N;i++){
+            if(vis[i] == false){
+                if(dfs(i,vis,rec,adj)) return false;
+            }
+        }
         return true;
     }
-    
-    bool canFinish(int n, vector<vector<int>>& grid) {
-        
-        vector<int> adj[n];
-        for(int i =0;i<grid.size();i++){
-           
-            int u = grid[i][1];
-            int v = grid[i][0];
-            adj[u].push_back(v);
-        }
-        
-    /* start dfs to check cycle exist in directed graph or not */
-    /* approach is if vertex forms backedge with any of its parent present in 
-        stack then cycle exist return false */
-         
-        vector<int> visited(n,0);
-        vector<int> recSt(n,0);
+};
