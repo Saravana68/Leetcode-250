c8e6c85ab74d2872a1aac8daa0bd214925b164e6
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2022-11-20 13:42:46 +0530

    278 ms

--- solutions/1642. Furthest Building You Can Reach.cpp
+++ solutions/1642. Furthest Building You Can Reach.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int sum = 0 ;
-        priority_queue<int,vector<int>,greater<int> >  q;
-        for(int i = 1;i<heights.size();i++){
-            if(heights[i]>heights[i-1]){
-                q.push(heights[i]-heights[i-1]);
-                if(q.size() > ladders){
-                    sum += q.top();
-                    q.pop();
-                }
-                if(sum > bricks)
-                  return i-1;
+    int furthestBuilding(vector<int>& arr, int bricks, int ladders) {
+        priority_queue<int,vector<int>,greater<int>> gapQueue;
+        int sum =0;
+        for(int i = 1; i<arr.size();i++){
+            if(arr[i] <= arr[i-1]) continue;
+            int gap = arr[i] - arr[i-1];
+            gapQueue.push(gap);
+            if(gapQueue.size() > ladders){
+                sum += gapQueue.top(); gapQueue.pop();
             }
+            if(sum > bricks) return i -1;
         }
-        return heights.size()-1;
+        return arr.size() -1 ;
     }
 };
