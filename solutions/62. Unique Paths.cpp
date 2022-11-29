ebd9059845a6c8df427e04de68bbac16faedca19
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2022-11-29 08:58:49 +0530

    6 ms

--- solutions/62. Unique Paths.cpp
+++ solutions/62. Unique Paths.cpp
@@ -11,7 +11,23 @@ public:
         return dp[i][j] = up + left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+        
+        vector<int> prev(n,0);
+        for(int i =0;i<m;i++){
+            vector<int> curr(n,0);
+            for(int j =0;j<n;j++){
+                if(i ==0 && j== 0){
+                    curr[j] = 1;
+                    continue;
+                }
+                int up =0 , left = 0;
+                if(i >0) up = prev[j];
+                if( j > 0) left = curr[j-1];
+                
+                curr[j] = up + left;
+            }
+            prev = curr;
+        }
+        return prev[n-1];
     }
 };
