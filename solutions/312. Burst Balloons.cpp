b967a8a82432d5b23a504276c99046953dfa8a46
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2023-01-12 20:28:13 +0530

    280 ms

--- solutions/312. Burst Balloons.cpp
+++ solutions/312. Burst Balloons.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
+    int solve(int i,int j,int arr[],vector<vector<int>> &dp){
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int maxi = INT_MIN;
+        for(int k = i;k<=j;k++){
+            int cost = arr[i-1] * arr[k] * arr[j+1];
+            cost += solve(i,k-1,arr,dp) + solve(k+1,j,arr,dp);
+            maxi = max(maxi,cost);
+        }
+        return dp[i][j] = maxi;
+    }
     int maxCoins(vector<int>& nums) {
-        
         int n = nums.size();
-        
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
         int arr[n+2];
-        
         arr[0] = 1;
-        
         arr[n+1] = 1;
+        for(int i = 0;i<nums.size();i++)
+            arr[i+1] = nums[i];
+        //return solve(1,n,arr,dp);
         
-        for(int i = 1; i <= n; i++)
-            arr[i] = nums[i-1];
-        
-        int dp[n+2][n+2];
-        memset(dp, 0, sizeof(dp));
-        
-        for(int length = 1; length <= n; length++)
-        {
-            for(int left = 1; left <= (n - length + 1); left++)
-            {
-                int right = left + length - 1;
-                
-                for(int i = left; i <= right; i++)
-                {
-                    dp[left][right] = max(dp[left][right], (arr[left-1] * arr[i] * arr[right+1]) + dp[left][i-1] + dp[i+1][right]);
+        for(int i = n;i>=1;i--){
+            for(int j = 1; j<=n;j++){
+                if(i > j) continue;
+                int maxi = INT_MIN;
+                for(int k = i;k<=j;k++){
+                    int cost = arr[i-1] * arr[k] * arr[j+1];
+                    cost += dp[i][k-1] + dp[k+1][j];
+                    maxi = max(maxi,cost);
                 }
+                dp[i][j] = maxi;
             }
         }
-        
-        return dp[1][n] ;
+        return dp[1][n];
     }
 };
