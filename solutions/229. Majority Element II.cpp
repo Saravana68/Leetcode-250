0237f5e35f984b7daf1da29b38e53e4b11257e76
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2023-08-23 12:10:38 +0530

    7 ms

--- solutions/229. Majority Element II.cpp
+++ solutions/229. Majority Element II.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int elem1 = -1 , elem2 = -1;
-        int count1 = 0, count2 = 0;
         
+        
+        // assuming - 1 as majority element and their frequency as 0
+        int cnt1 = 0, cnt2 = 0;
+        int num1 = -1, num2 = -1;
+        
+        //output array 
+        vector<int> ans = {};
+        
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == num1) cnt1++;
+            else if(nums[i] == num2) cnt2++;
+            else if(cnt1 == 0){ cnt1 = 1; num1 = nums[i];}
+            else if(cnt2 == 0){cnt2 = 1; num2 = nums[i];}
+            else{cnt1--; cnt2--;}
+        }
+        
+        // store the frequency of chosen elements.
+        cnt1 = cnt2 = 0;
         for(int i =0;i<nums.size();i++){
-            
-            if(nums[i] == elem1) {count1++;}
-            else if(nums[i] == elem2) {count2++;}
-            else if(count1 == 0 ){elem1 = nums[i]; count1++; }
-            else if(count2 == 0) {elem2 = nums[i]; count2++; }
-            else{count1--; count2--;}
+            if(nums[i] == num1) cnt1++;
+            else if(nums[i] == num2) cnt2++;
         }
         
-        count1 = 0; count2 = 0;
-        for(int x : nums){
-            if(x == elem1) count1++;
-            else if(x == elem2) count2++;
-        }
-        vector<int> ans;
-        if(count1 > nums.size() / 3) ans.push_back(elem1);
-        if(count2 > nums.size() / 3) ans.push_back(elem2);
+        // check their frequency are greater than one third of input array size.
+        int threshold_value = int(nums.size()) / 3;
+        if(cnt1 > threshold_value) ans.push_back(num1);
+        if(cnt2 > threshold_value) ans.push_back(num2);
+        
         return ans;
-    }
+    }
 };
