050de0c9ceaadf25ad9ec30dcf1d55e8d2344d96
Saravana Kumar <63772127+Saravana68@users.noreply.github.com>
2023-09-03 18:13:10 +0530

    0 ms

--- solutions/70. Climbing Stairs.cpp
+++ solutions/70. Climbing Stairs.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    int solve(int n, vector<long long int> &dp){
-        if(n<=2) return n;
+    int solve(int n,vector<int> &dp){
+        if(n == 0) return 1;
+        if(n < 0) return 0;
         if(dp[n] != -1) return dp[n];
         return dp[n] = solve(n-1,dp) + solve(n-2,dp);
     }
     int climbStairs(int n) {
-        if(n <= 1) return 1;
-        int prev2 = 1;
-        int prev = 1;
-        int curr = 0;
-        for(int i = 2;i<=n;i++){
-            curr = prev + prev2;
-            prev2 = prev;
-            prev = curr;
-        }
-        return curr;
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
